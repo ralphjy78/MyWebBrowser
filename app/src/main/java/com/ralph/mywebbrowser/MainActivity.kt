b66dd9580d1f8d3275cc94e59f200e760b5be626
ralphjy78 <ralphjy@gmail.com>
2019-02-12 23:53:53 +0900

    스텝2 완료

--- app/src/main/java/com/ralph/mywebbrowser/MainActivity.kt
+++ app/src/main/java/com/ralph/mywebbrowser/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ralph.mywebbrowser
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.inputmethod.EditorInfo
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,5 +18,22 @@ class MainActivity : AppCompatActivity() {
         }
 
         webView.loadUrl("http://www.google.com")
+
+        editTextUrl.setOnEditorActionListener { _, actionId, _ ->
+            if(actionId == EditorInfo.IME_ACTION_SEARCH) {
+                webView.loadUrl(editTextUrl.text.toString())
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
