1e2568caf53c5a7c7b739bd6cf7a7f492fd47aef
ralphjy78 <ralphjy@gmail.com>
2019-02-16 00:04:31 +0900

    나만의 브라우저 완료

--- app/src/main/java/com/ralph/mywebbrowser/MainActivity.kt
+++ app/src/main/java/com/ralph/mywebbrowser/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.ralph.mywebbrowser
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +34,8 @@ class MainActivity : AppCompatActivity() {
                 false
             }
         }
+
+        registerForContextMenu(webView)
     }
 
     override fun onBackPressed() {
@@ -35,5 +44,74 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.action_google, R.id.action_home -> {
+                webView.loadUrl("http://www.google.com")
+                return true
+            }
+            R.id.action_naver -> {
+                webView.loadUrl("http://www.naver.com")
+                return true
+            }
+            R.id.action_daum -> {
+                webView.loadUrl("http://www.daum.net")
+                return true
+            }
+            R.id.action_call -> {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:010-3004-3777")
+
+                if(intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+
+                return true
+            }
+            R.id.action_sms -> {
+                // SMS
+                sendSMS("010-3004-3777", webView.url)
+                return true
+            }
+            R.id.action_email -> {
+                // email
+                email("ralphjy@gmail.com", "kotlin android test", webView.url)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+
+//        return true
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        menuInflater.inflate(R.menu.context, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.action_share -> {
+                // 페이지 공유
+                share(webView.url)
+                return true
+            }
+            R.id.action_browser -> {
+                // 기본 브라우저에서 열기
+                browse(webView.url)
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 }
